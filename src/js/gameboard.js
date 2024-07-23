8a9804fb1a9583a2177a221a9066cf526804b438
foxane <65512187+foxane@users.noreply.github.com>
2024-07-23 18:22:33 +0700

    Refactor placeship method, add initial receive attack method

--- src/js/gameboard.js
+++ src/js/gameboard.js
@@ -1,8 +1,11 @@
 import Ship from './ship';
 
 export default class Gameboard {
+  missedAttacks = [];
+  hitAttacks = [];
+  size = 10;
+
   constructor() {
-    this.size = 10;
     this.create2DArray(this.size);
   }
 
@@ -13,48 +16,39 @@ export default class Gameboard {
   }
 
   // NOTE: Add validation for out of bound coordinate
-  // eslint-disable-next-line max-statements
   placeShip(start, end) {
-    const data = {
-      axis: 'null',
-      cellSequence: [],
-      colEnd: end[1],
-      colStart: start[1],
-      rowEnd: end[0],
-      rowStart: start[0],
-      shipLength: 0,
-    };
+    // Coordinate validation: start is bigger than end
+    if (start[0] > end[0] || start[1] > end[1]) return false;
 
-    // Coordinate validation : start is bigger than end
-    if (data.rowStart > data.rowEnd || data.colStart > data.colEnd)
-      return false;
-
-    // Data properties
-    data.axis = data.rowEnd - data.rowStart > 0 ? 'hor' : 'ver';
-    data.shipLength =
-      data.axis === 'hor'
-        ? data.rowEnd - data.rowStart + 1
-        : data.colEnd - data.colStart + 1;
+    const [[rowStart, colStart], [rowEnd, colEnd]] = [start, end],
+      axis = rowEnd - rowStart > 0 ? 'hor' : 'ver',
+      cellSequence = [],
+      length = axis === 'hor' ? rowEnd - rowStart + 1 : colEnd - colStart + 1,
+      newShip = new Ship(length);
 
     // Populate cell sequence
-    for (let i = 0; i < data.shipLength; i += 1) {
-      const col = data.axis === 'ver' ? data.colStart + i : data.colStart,
-        row = data.axis === 'hor' ? data.rowStart + i : data.rowStart;
-
-      // Cell is occupied, break function
-      if (this.board[row][col] !== null) return false;
-
-      data.cellSequence.push([row, col]);
+    for (let i = 0; i < length; i += 1) {
+      const col = axis === 'ver' ? colStart + i : colStart,
+        row = axis === 'hor' ? rowStart + i : rowStart;
+      if (this.board[row][col] !== null) {
+        // Break function when cell is occupied
+        return false;
+      }
+      cellSequence.push([row, col]);
     }
 
-    // Finally placing ship
-    {
-      const newShip = new Ship(data.shipLength);
-      data.cellSequence.forEach(
-        ([row, col]) => (this.board[row][col] = newShip),
-      );
+    cellSequence.forEach(([row, col]) => (this.board[row][col] = newShip));
+    return true;
+  }
+
+  receiveAttack([row, col]) {
+    if (this.board[row][col] === null) {
+      this.missedAttacks.push([row, col]);
+      return false;
     }
 
+    this.board[row][col].hit();
+    this.hitAttacks.push([row, col]);
     return true;
   }
 }

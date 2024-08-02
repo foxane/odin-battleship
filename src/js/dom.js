0e09981c6e3888851ed831d70533a99f5a65879f
foxane <65512187+foxane@users.noreply.github.com>
2024-08-02 10:26:27 +0700

    Changed functions to class

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,40 +1,56 @@
-const BOARD_SIZE = 10;
+import Gameboard from './gameboard';
 
-const createBoard = (callback) => {
-  const board = document.createElement('div');
-  board.classList.add('board');
+// Helper fns, inaccessible from outside
+const addCellAttributes = (cell, [row, col], callback) => {
+  cell.classList.add('cell');
+  cell.dataset.row = row;
+  cell.dataset.col = col;
+  cell.dataset.status = 'null';
 
-  // Add inactive class if computer (for init)
-  if (callback) board.classList.add('inactive');
+  if (callback) {
+    cell.addEventListener('click', () => {
+      callback(cell);
+    });
+    cell.dataset.player = false;
+  } else {
+    cell.dataset.player = true;
+  }
+};
 
-  for (let row = 0; row < BOARD_SIZE; row += 1) {
-    for (let col = 0; col < BOARD_SIZE; col += 1) {
-      const cell = document.createElement('div');
-      cell.classList.add('cell');
-      cell.dataset.row = row;
-      cell.dataset.col = col;
-      cell.dataset.status = 'null';
+export default class Dom {
+  boardSize = Gameboard.size;
 
-      // Callback is provided
-      if (callback)
-        cell.addEventListener('click', () => {
-          callback(cell);
-        });
+  constructor() {
+    this.playerElement = document.querySelector('.player');
+    this.computerElement = document.querySelector('.computer');
+  }
 
-      board.appendChild(cell);
+  createBoard(callback) {
+    const board = document.createElement('div');
+    if (callback) {
+      board.classList.add('board', 'inactive');
+    } else {
+      board.classList.add('board');
+    }
+
+    for (let row = 0; row < this.boardSize; row += 1) {
+      for (let col = 0; col < this.boardSize; col += 1) {
+        const cell = document.createElement('div');
+        addCellAttributes(cell, [row, col], callback);
+
+        board.appendChild(cell);
+      }
+    }
+    return board;
+  }
+
+  renderBoard(shipCoordinates) {
+    this.playerElement.innerHTML = '';
+    for (const [row, col] of shipCoordinates) {
+      const cell = document.querySelector(
+        `.cell[data-row="${row}"][data-col="${col}"][data-player="true"]`,
+      );
+      cell.style.backgroundColor = 'blue';
     }
   }
-
-  return board;
-};
-
-const renderBoard = (shipCoordinates) => {
-  for (const [row, col] of shipCoordinates) {
-    const cell = document.querySelector(
-      `.cell[data-row="${row}"][data-col="${col}"]`,
-    );
-    cell.style.backgroundColor = 'blue';
-  }
-};
-
-export { createBoard, renderBoard };
+}
